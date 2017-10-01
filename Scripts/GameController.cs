1f36037dc2ee61f5b56bdefa571edd67012adb15
Hugo Puyol <hugo_asterix@hotmail.com>
2017-10-01 07:52:46 +0200

    Update GameController.cs

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -3,8 +3,33 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
   
   public ScoreZone[] zones;
+  public List<Player> players;
   
   public Start(){
+    //initialize players
+  }
   
+  public void ScorePlayer(string name, int score){
+    Player player = players.Find(x => x.name == name);
+    player.score += score;
+    UpdateScore():
+  }
+  
+  void UpdateScore(){
+    string score = "Score Table\n";
+    foreach(Player player in players){
+      score += player.name + "\t" + player.score;
+    }
+    Debug.Log(score);
+  }
+  
+  public class Player {
+    public string player;
+    public int score;
+  
+    public Player(string name){
+      this.player = name;
+      this.score = 0;
+    }
   }
 }
